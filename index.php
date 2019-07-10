6a669137f1c35f85635b8403e48285e765418e75
Marcos Oliveira <marcos.oliveira098@gmail.com>
2019-07-09 21:50:41 -0400

    final 1.0.1

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <title>Teste Portas</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="shortcut icon" href="https://cdn4.iconfinder.com/data/icons/web-and-seo-icon-set/512/global-search-512.png">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <link href="css/style.css" rel="stylesheet">
